a20edcfe4f27782b29d81b130cbcc60b11582cdf
issuereporter1234 <118681835+issuereporter1234@users.noreply.github.com>
2025-04-17 19:46:32 +0200

    Implement repeatString function
    Add two parameters: startString and num
    Return "ERROR" if num is negative
    Repeat the input string num times and return the result

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(startString, num) {
+    if (num < 0){
+        return "ERROR"
+    }
 
+    else{
+    for(endString= ""; num > 0; --num){
+        endString = endString + startString
+    }
+    }
+    
+    return endString
 };
-
+repeatString('hey', )
 // Do not edit below this line
 module.exports = repeatString;
