b285501775f7bc788e5d50863d4c9c203c38f7bb
issuereporter1234 <118681835+issuereporter1234@users.noreply.github.com>
2025-04-18 16:13:27 +0200

    Calculator summ two numbers subtract two numbers sum numbers in an array multiply numbers in an array raise the number x to the power of a number y cumpute the factorial to the number of x

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
+const add = function(x, y) {
+  return x + y
 	
 };
 
-const subtract = function() {
+const subtract = function(x, y) {
+  return x - y
+
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let zbir = 0
+  for (let i = 0; i < array.length; i++ ) {
+    zbir += array[i];
+    
+  }
+	return zbir
+};
+
+const multiply = function(array) {
+  let quotient = 1
+  for (let i = 0; i < array.length; i++){
+    quotient *= array[i];
+  }
+  return quotient
+};
+
+const power = function(x, y) {
+  return x ** y
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+  let fac = 1
+  if (x == 0 ){x = 1}
+  else{
+    
+    for (let counter = 1; counter <= x; ++counter)
+      fac *= counter
+    }
+  
+	return fac
 };
 
 // Do not edit below this line
