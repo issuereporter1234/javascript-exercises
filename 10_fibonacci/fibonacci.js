8c584a011872f612d2531ea8d735ee67d8855b48
issuereporter1234 <118681835+issuereporter1234@users.noreply.github.com>
2025-04-18 21:06:32 +0200

    Fibonacci sequence use the formula  F(n)=F(n−1)+F(n−2) and store results in an array return "OOPS" if the argument is negetive

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
-};
-
+//formula: F(n)=F(n−1)+F(n−2)
+const fibonacci = function(count) {
+    if (count < 0){return "OOPS"}
+    const fib = [0, 1];
+    for (let i = 2; i <= count; i++) {
+       fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[count]
+}
 // Do not edit below this line
 module.exports = fibonacci;
