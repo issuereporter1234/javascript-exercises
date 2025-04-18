bb9a2dc37d66f6d25a0ed6fba46ace7924a18b20
issuereporter1234 <118681835+issuereporter1234@users.noreply.github.com>
2025-04-18 11:35:29 +0200

    Implement sumAll function - Add number1 and number2 as parameters - Return "ERROR" if:    1. Either number is negative    2. Either number is not an integer    3. Either value is not a number - If number1 is smaller than number2:    Use for loop to sum numbers from number1 to number2 - If number1 is larger than number2:    Use for loop to sum numbers from number2 to number1 - Return the total sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(number1 , number2) {
+    let sum = 0
+    if ((number1 < 0) || (number2 < 0) || !Number.isInteger(number1) || !Number.isInteger(number2) || isNaN(number1) || isNaN(number2)){
+        return "ERROR"
+    }
+    else if (number1 < number2){
+    
+    for(let counter = number1; number2 >= counter; ++counter){
+        sum += counter
+    }
+}
+    else {
+        for (counter = number2; number1 >= counter; ++counter){
+            sum += counter
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
