e5be9007424fb2b471e1a46950a3928fb0c308f0
issuereporter1234 <118681835+issuereporter1234@users.noreply.github.com>
2025-04-18 20:10:57 +0200

    Implement palindrome checker: - Add `str` parameter to the function - Remove all non-alphanumeric characters - Convert to lowercase and remove spaces - Compare cleaned string with its reverse - Return true if they match, else false

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    str = str.replace(/\s/g, '')
+    
+    if (str.split("").reverse().join("") == str){
+        return true
+    }
+    else { return false}
 };
 
 // Do not edit below this line
