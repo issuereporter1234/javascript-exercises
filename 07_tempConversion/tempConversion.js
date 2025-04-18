50cc10226b77c3441bd712c5e03766ce4e1630c8
issuereporter1234 <118681835+issuereporter1234@users.noreply.github.com>
2025-04-18 14:45:51 +0200

    Calculate farenheit and celsius
    celsius:
    use the formula °C = (°F - 32) × 5/9
    round the number on one decimal
    
    farenheit:
    use the formula °F = (°C × 9/5) + 32
    round on one decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
-};
+// Fourmula °C = (°F - 32) × 5/9
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius = (farenheit - 32) * 5 / 9
+  celsius = Math.round(celsius * 10) / 10
+return celsius
+};
+// Formula : °F = (°C × 9/5) + 32
+const convertToFahrenheit = function(celsius) {
+  let farenheit = (celsius * 9 / 5) + 32
+  farenheit = Math.round(farenheit * 10) / 10
+  return farenheit
 };
 
 // Do not edit below this line
